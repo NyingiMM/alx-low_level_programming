b7a2816065c82cc6a05244b9664b2153cc5db9cb
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-06-30 14:53:28 -0700

    gut

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>                                                                   
-/**                                                                                  
- *  *main - Entry point                                                                 
- *   *Describe: Prints "\"Programming is like building a multilingual puzzle"            
- *    * Return: 0                                                                         
- *     */                                                                                  
-int main(void)                                                                       
-{                                                                                    
-	        puts("\"Programming is like building a multilingual puzzle");                
-		        return(0);                                                                   
+#include <stdio.h>
+/
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	Puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
