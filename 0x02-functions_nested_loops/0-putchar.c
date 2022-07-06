a9890c762e1ea89c3f7692446091d5e4ff91fe4a
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-06 00:10:44 -0700

    Jau

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * main - Entry point
  *
@@ -9,7 +8,7 @@
  */
 int main(void)
 {
-	_putcha('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,6 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n')
+	_putchar('\n');
 	return (0);
 }
