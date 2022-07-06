2c2327a23100026ac532bc15c096ad9e79aef0b5
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-05 18:19:07 -0700

    Ful

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include<_putchar>
+#include "main.h"
+
 
 /**
  * main - Entry point
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - main function
+ *
+ * @c: character to compare
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
