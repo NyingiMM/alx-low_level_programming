268b359c151a15f07cf7a573fd05ecb5549d0a64
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-08 18:07:58 -0700

    vee

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 9;
+	i = 98;
 	positive_or_negative(i);
 
-	return(0);
+	Return (0);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
 /**
- * largest number - returns the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
 
-int largest_number(int a, intb, int c)
+int largest_number(int a, int b, int c)
 {
 	int largest;
-
-	if (a > b && a > c)
+	if (a > b)
 	{
-		largest = a;
+		if (a > c)
+		{
+			largest = a;
+		}
+		else
+		{
+			largest = c;
+		}
 	}
-	else if (b > c && b > a)
+	else if (b > c)
 	{
 		largest = b;
-	}
-	else if (c > b)
-	{
-		largest = c;
 	}
 	else
 	{
-		largest = b;
+		largest = c;
 	}
 
 	return (largest);
-}
