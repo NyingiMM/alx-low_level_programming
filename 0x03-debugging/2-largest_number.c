348097fcb2b829186c569429153056d293c801c2
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-11 04:24:37 -0700

    ja

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,7 +6,6 @@
  * @b: second integer
  * @c: third integer
  * Return: largest number
- *
  */
 
 int largest_number(int a, int b, int c)
@@ -28,6 +27,6 @@ int largest_number(int a, int b, int c)
 	else
 	{
 		largest = b;
-
+	}
 	return (largest);
 }
