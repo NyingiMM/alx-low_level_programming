f407dff38733acc17f3435beeaf332565882ebcf
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-11 03:46:03 -0700

    Didid

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,30 +6,25 @@
  * @b: second integer
  * @c: third integer
  * Return: largest number
+ *
  */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && a > c)
 	{
-		if (a > c)
-		{
-			largest = a;
-		}
-		else
-		{
-			largest = c;
-		}
+		largest = a;
 	}
-	else if (b > c)
+	else if (b > c && b > a)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b && c > a)
 	{
 		largest = c;
 	}
 
 	return (largest);
+}
