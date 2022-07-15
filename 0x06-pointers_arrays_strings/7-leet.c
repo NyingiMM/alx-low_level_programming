999cf8588dd3a8cbf54d692c872992d74e1d4a97
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-15 15:44:26 -0700

    quero

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,31 +1,22 @@
-nclude "main.h"
+#include "main.h"
 /**
- * leet - encodes a string
- * @s: pointer to string
- * Return: pointer to s
+ * leet - encodes a string into 1337
+ * @str: string to encode
+ * Return: pointer to string
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int stringCount, leetCount;
-	char leetLetters[] = "aAeEoOtTlL";
-	char leetNums[] = "4433007711";
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	/* scan through a string using stringcount */
-	stringCount = 0;
-
-	while (s[stringCount] != '\0')
-		/* check whether leetLetters are found */
+	while (str[++index1])
 	{
-		leetCount = 0;
-		while (leetCount < 10)
+		for (index2 = 0; index2 <= 7; index2++)
 		{
-			if (leetLetters[leetCount] == s[stringCount])
-			{
-				s[stringCount] = leetNums[leetCount];
-			}
-			leetCount++;
+			if (str[index1] == leet[index2] ||
+					str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
 		}
-		stringCount++;
 	}
-	return (s);
+	return (str);
 }
