43d6cdf9175c3f6a4b37ed19963a3f5d4476a796
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-15 18:58:34 -0700

    hh

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit - determine if it is digit
- *
+ * _isdigit - determine if it is digiT
  * @c: character to determine
  * Return: successful
  */
 int _isdigit(int c)
 {
-	if ((c >= 48)  && c <= '57)
+	if ((c >= 48)  && (c <= 57))
 		return (1);
 	return (0);
 }
