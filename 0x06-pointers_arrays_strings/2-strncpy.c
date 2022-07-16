75579394f4acb3719bd70d842c6a94c0d7a8d92e
Martin Nyingi Mwinzi <mnmwinzi1@gmail.com>
2022-07-16 04:19:54 -0700

    g

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - C function that copies a string
+ * terminates null byte
+ * works similar to the 'strncpy' library function
+ * @dest: buffer storing the the string copy
+ * @src: the sources string
+ * @n: max number of copied bytes
+ * Return: return
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
